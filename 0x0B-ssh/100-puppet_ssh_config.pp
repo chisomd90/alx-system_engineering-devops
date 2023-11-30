128025c85db5d66e3cb2f67aaaf1f6937b31d5b6
chisomd90 <chisomd90@gmail.com>
2023-11-30 01:54:12 +0000

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# a manifest that kills a process named killmenow.
+
+file { 'etc/ssh/ssh_config':
+       ensure => present'
+
+content =>"
+
+        #SSH client configuration
+        host*
+        IdentityFile ~/.ssh/school
+        passwordAuthentication no
+
+}
