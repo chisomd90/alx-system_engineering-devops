f20e9f1f8c9379ef944322dbb5645ebf6bdcdf44
chisomd90 <chisomd90@gmail.com>
2023-11-30 00:24:38 +0000

    Execute a command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-# File: 1-install_a_package.pp
+#Using Puppet, create a file in /tmp.
 
+package { 'python3-pip':
+  ensure => 'installed',
+}
 package { 'flask':
-  ensure  => '2.1.0',
-  provider => 'pip3',
+  ensure   => '2.1.0',
+  #provider => 'pip3',
+  require  => Package['python3-pip'],
+  provider => 'pip3'
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  #provider => 'pip3',
+  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
